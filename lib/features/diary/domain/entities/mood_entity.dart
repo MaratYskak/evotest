794cce6734078dea9d5545cfa899b61b4c955aa9
Marat Yskak <yskakmarat2@gmail.com>
2026-02-04 23:09:20 +0500

    add mood entity

---
+++ lib/features/diary/domain/entities/mood_entity.dart
@@ -0,0 +1,15 @@
+class MoodEntity {
+  final String feeling;
+  final int stressLevel;
+  final int selfEsteemLevel;
+  final String note;
+  final DateTime date;
+
+  MoodEntity({
+    required this.feeling,
+    required this.stressLevel,
+    required this.selfEsteemLevel,
+    required this.note,
+    required this.date,
+  });
+}
